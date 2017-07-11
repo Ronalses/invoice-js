8a450ecc44e18676229e8d06a44a7670b599f4d3
Ronalses Aguilar <ronalses.developer@gmail.com>
2017-07-11 15:45:55 -0400

    add controlers to apiFake

---
+++ apiFake/controlers/Client.js
@@ -0,0 +1,70 @@
+const fs = require('fs')
+
+exports.add = (req, res) => {
+  let json = JSON.parse(fs.readFileSync(`${__dirname}/../BDFake/clients.json`, 'utf8'))
+  json.clients.push(req.body)
+  console.log(json)
+  json = JSON.stringify(json, null, '    ')
+  fs.writeFile(`${__dirname}/BDFake/clients.json`, json, 'utf8', () => {
+    res.status(200).json({ message: 'user create' })
+  })
+}
+
+exports.get = (req, res) => {
+  console.log(req.params.ci)
+  let ci = req.params.ci
+  let json = JSON.parse(fs.readFileSync(`${__dirname}/../BDFake/clients.json`, 'utf8'))
+  let clients = json.clients
+  for (let i in clients) {
+    if (ci === clients[i].ci) {
+      return res.status(200).json(clients[i])
+    }
+  }
+  res.status(200).json({ message: 'send user' })
+}
+
+exports.edit = (req, res) => {
+  // if boby is not null
+  if (JSON.stringify(req.body) === '{}') {
+    return res.status(400).json({ error: true, message: 'error ci' })
+  }
+
+  let ci = req.params.ci
+  let json = JSON.parse(fs.readFileSync(`${__dirname}/../BDFake/clients.json`, 'utf8'))
+  let client = req.body
+  let clients = json.clients
+
+  for (let i in clients) {
+    // Search client for ci
+    if (ci === clients[i].ci) {
+      clients[i] = client
+      json.clients = clients
+      json = JSON.stringify(json, null, '    ')
+      fs.writeFile(`${__dirname}/BDFake/clients.json`, json, 'utf8', () => {
+        return res.status(200).json({ message: 'user update' })
+      })
+    }
+  }
+}
+
+exports.delete = (req, res) => {
+  console.log(req.params.ci)
+  let ci = req.params.ci
+  let json = JSON.parse(fs.readFileSync(`${__dirname}/../BDFake/clients.json`, 'utf8'))
+  let clients = json.clients
+  clients = clients.filter(function (client) {
+    if (client.ci !== ci) {
+      return client
+    }
+  })
+  json.clients = clients
+  json = JSON.stringify(json, null, '    ')
+  fs.writeFile(`${__dirname}/BDFake/clients.json`, json, 'utf8', () => {
+    return res.status(200).json({ message: 'user delete' })
+  })
+}
+
+exports.all = (req, res) => {
+  let clients = JSON.parse(fs.readFileSync(`${__dirname}/../BDFake/clients.json`, 'utf8'))
+  res.status(200).json(clients)
+}
--- apiFake/index.js
+++ apiFake/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const fs = require('fs')
+const Client = require('./controlers/Client')
 
 const router = express.Router()
 
@@ -35,62 +36,11 @@ router.route('/client')
   })
 
 router.route('/client/:ci')
-  .get((req, res) => {
-    console.log(req.params.ci)
-    let ci = req.params.ci
-    let json = JSON.parse(fs.readFileSync(`${__dirname}/BDFake/clients.json`, 'utf8'))
-    let clients = json.clients
-    for (let i in clients) {
-      if (ci === clients[i].ci) {
-        return res.status(200).json(clients[i])
-      }
-    }
-    res.status(200).json({ message: 'Hola client' })
-  })
-  .put((req, res) => {
-    // if boby is not null
-    if (JSON.stringify(req.body) === '{}') {
-      return res.status(400).json({ error: true, message: 'error ci' })
-    }
-
-    let ci = req.params.ci
-    let json = JSON.parse(fs.readFileSync(`${__dirname}/BDFake/clients.json`, 'utf8'))
-    let client = req.body
-    let clients = json.clients
-
-    for (let i in clients) {
-      // Search client for ci
-      if (ci === clients[i].ci) {
-        clients[i] = client
-        json.clients = clients
-        json = JSON.stringify(json, null, '    ')
-        fs.writeFile(`${__dirname}/BDFake/clients.json`, json, 'utf8', () => {
-          return res.status(200).json({ message: 'user update' })
-        })
-      }
-    }
-  })
-  .delete((req, res) => {
-    console.log(req.params.ci)
-    let ci = req.params.ci
-    let json = JSON.parse(fs.readFileSync(`${__dirname}/BDFake/clients.json`, 'utf8'))
-    let clients = json.clients
-    clients = clients.filter(function (client) {
-      if (client.ci !== ci) {
-        return client
-      }
-    })
-    json.clients = clients
-    json = JSON.stringify(json, null, '    ')
-    fs.writeFile(`${__dirname}/BDFake/clients.json`, json, 'utf8', () => {
-      return res.status(200).json({ message: 'user delete' })
-    })
-  })
+  .get(Client.get)
+  .put(Client.edit)
+  .delete(Client.delete)
 
 router.route('/clients')
-  .get((req, res) => {
-    let clients = JSON.parse(fs.readFileSync(`${__dirname}/BDFake/clients.json`, 'utf8'))
-    res.status(200).json(clients)
-  })
+  .get(Client.all)
 
 module.exports = router
